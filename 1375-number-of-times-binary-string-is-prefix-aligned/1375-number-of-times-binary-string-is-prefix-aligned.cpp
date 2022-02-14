a77cb644b7f2c08dd3465a3b9d69f665db40f52e
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-02-14 09:28:32 +0530

    Time: 51 ms (96.72%), Space: 38.3 MB (93.45%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    int numTimesAllBlue(vector<int>& flips) {
-        // vector<pair<int,int>>v;
-        // for(int i=0;i<flips.size();i++){
-        //     v.push_back({flips[i],i});
-        // }
-        // sort(v.begin(),v.end());
-        priority_queue<int,vector<int>,greater<int>>pq;
-        priority_queue<int>pqq;
-        for(int i=0;i<flips.size();i++){
-            pq.push(i);
-        }
+    int numTimesAllBlue(vector<int>& flips) 
+    {
+        int count=0;
+        int max_val=0;
         int n=flips.size();
-        int ans=0;
-        vector<bool>vis(n,false);
-        for(int i=0;i<n;i++){
-            pqq.push(flips[i]-1);
-            vis[flips[i]-1]=true;
-            while(pq.empty()==false and vis[pq.top()]==true){
-                pq.pop();
-            }
-            if(pq.empty()==false and pqq.top()<pq.top()){
-                ans++;
-            }
-            else if(pq.empty()==false and pqq.top()>pq.top()){
-                continue;
-            }else if(pq.empty()==true or pq.top()>flips[i]-1){
-                ans++;
+        for(int i=0;i<n;i++)
+        {
+            max_val=max(max_val,flips[i]-1);
+            if(i==max_val)
+            {
+                count++;
             }
         }
-        // return ans;
-        return ans;
+        return count;
     }
 };
