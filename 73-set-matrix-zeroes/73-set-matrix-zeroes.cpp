81cb9993a4aea656852d9cf162db46e64e74d577
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-02-14 13:53:51 +0530

    Time: 97 ms (5.59%), Space: 13.5 MB (11.06%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& arr) {
+        int c=0;
+        int n=arr.size();
+        int m=arr[0].size();
+        for(int i=0;i<n;i++){
+            if(arr[i][0]==0){
+                c++;
+            }
+            for(int j=1;j<m;j++){
+                if(arr[i][j]==0){
+                    arr[i][0]=0;
+                    arr[0][j]=0;
+                }
+            }
+            
+        }
+        for(auto x:arr){
+            for(auto y:x){
+                cout<<y<<" ";
+            }
+            cout<<endl;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=1;j--){
+                if(arr[i][0]==0 or arr[0][j]==0){
+                    arr[i][j]=0;
+                }
+            }
+            if(c>0){
+                arr[i][0]=0;
+            }
+        }
+        
+    }
+};
