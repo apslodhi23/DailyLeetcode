f48c0ca858aafb5be90212828e442349300a5048
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-02-15 14:03:40 +0530

    Time: 8 ms (14.36%), Space: 9.1 MB (72.71%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k=n+m;
+        int i=m-1;
+        int j=n-1;
+        int kk=k-1;
+        while(i>=0 and j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[kk]=nums1[i];
+                kk--;
+                i--;
+            }else{
+                nums1[kk]=nums2[j];
+                j--;
+                kk--;
+            }
+        }
+        while(j>=0){
+            nums1[kk]=nums2[j];
+            kk--;
+            j--;
+        }
+    }
+};
