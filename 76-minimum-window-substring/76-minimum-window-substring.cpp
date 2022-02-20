41ee5c78a32bfa7af23a38a6c24289c54f345853
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-02-20 09:51:33 +0530

    Time: 50 ms (25.75%), Space: 7.6 MB (93.27%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int m=t.size();
+        int n=s.size();
+        if(n<m){
+            return "";
+        }
+        map<char,int>m1;
+        for(auto x:t){
+            m1[x]++;
+        }
+        int ss=0;
+        int ans=INT_MAX;
+        int i=0,j=0;
+        int c=m1.size();
+        while(i<n){
+            if(m1.find(s[i])!=m1.end()){
+                m1[s[i]]--;
+                if(m1[s[i]]==0){
+                    c--;
+                }
+            }
+            while(c==0){
+                if(ans>i-j+1){
+                    ans=i-j+1;
+                    ss=j;
+                }
+                if(m1.find(s[j])!=m1.end()){
+                    m1[s[j]]++;
+                    if(m1[s[j]]>0){
+                        c++;
+                    }
+                }
+                j++;
+            }
+            i++;
+        }
+        if(ans==INT_MAX){
+            return "";
+        }
+        return s.substr(ss,ans);
+    }
+};
