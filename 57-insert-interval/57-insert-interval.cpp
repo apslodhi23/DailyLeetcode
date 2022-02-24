13163cb705d55683398bbf2d1b09f802d104d809
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-02-24 11:02:51 +0530

    Time: 35 ms (21.95%), Space: 17.1 MB (76.73%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -16,6 +16,45 @@ public:
             }
         }
         ans.push_back({v0,v1});
+        // return ans;
+        // vector<vector<int>>ans;
+        // if(arr.size()==0){
+        //     ans.push_back(c);
+        //     return ans;
+        // }
+        // int i =0;
+        // for(;i<arr.size();i++){
+        //     if(arr[i][1]>=c[0]){
+        //         break;
+        //     }else{
+        //         ans.push_back(arr[i]);
+        //     }
+        // }
+        // if(i==0 and arr[0][]>c[0]){
+        //     ans.push_back(c);
+        //     for(auto x:arr){
+        //         ans.push_back(x);
+        //     }
+        //     return ans;
+        // }
+        // if(i==arr.size()){
+        //     ans.push_back(c);
+        //     return ans;
+        // }
+        // int v0=min(arr[i][0],c[0]);
+        // int v1=max(arr[i][1],c[1]);
+        // i++;
+        // for(;i<arr.size();i++){
+        //     if(arr[i][0]<=v1){
+        //         v1=max(v1,arr[i][1]);
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // ans.push_back({v0,v1});
+        // for(;i<arr.size();i++){
+        //     ans.push_back(arr[i]);
+        // }
         return ans;
     }
 };
