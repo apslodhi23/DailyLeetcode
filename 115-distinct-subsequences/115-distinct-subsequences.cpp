3a8784900a182baf113be596978537708c1ed0ff
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-07 08:58:56 +0530

    Time: 91 ms (15.93%), Space: 18.3 MB (56.31%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,16 +3,11 @@ public:
     int mod=1e9+7;
     int numDistinct(string s, string t) {
         int m = t.length(), n = s.length();
-        vector<int> cur(m + 1, 0);
-        cur[0] = 1;
-        for (int j = 1; j <= n; j++) { 
-            int pre = 1;
-            for (int i = 1; i <= m; i++) {
-                int temp = cur[i];
-                cur[i] = (cur[i] + (t[i - 1] == s[j - 1] ? pre : 0))%mod;
-                pre = temp;
-            }
-        }
-        return cur[m];
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= m; i++)
+                dp[i][j] = (dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0))%mod;
+        return dp[m][n];
     }
 };
