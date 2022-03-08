199aee9be1fcb270b71a53fed9db148e95641856
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-08 15:19:04 +0530

    Time: 72 ms (7.81%), Space: 39.2 MB (5.02%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        vector<int>color(graph.size(),-1);
-        for(int i=0;i<graph.size();i++){
-            if(color[i]==-1){
-                color[i]=0;
-                queue<int>q;
-                q.push(i);
-                while(!q.empty()){
-                    int x=q.front();
-                    q.pop();
-                    for(auto j:graph[x]){
-                        if(color[j]==-1){
-                            color[j]=!color[x];
-                            q.push(j);
-                        }
-                        else 
-                        {
-                            if(color[j]==color[x]){
-                                return false;
-                            }
-                        }
-                    }
+    
+    /* using bfs  */
+    
+
+    //  bool isBipartite(vector<vector<int>>& arr) {
+    //     int n=arr.size();
+    //     vector<int>ans(n,-1);
+    //     for(int i=0;i<n;i++){
+    //         if(ans[i]==-1){
+    //             queue<int>q;
+    //             ans[i]=0;
+    //             q.push(i);
+    //             while(!q.empty()){
+    //                 int t=q.front();
+    //                 q.pop();
+    //                 for(auto x:arr[t]){
+    //                     if(ans[x]==-1){
+    //                         ans[x]=!ans[t];
+    //                         q.push(x);
+    //                     }else if(ans[x]==ans[t]){
+    //                         return false;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    
+    
+    /* using dfs  */
+    bool fun(vector<vector<int>>arr,int i,vector<int>&ans){
+        for(auto x:arr[i]){
+            if(ans[x]==-1){
+                ans[x]=!ans[i];
+                if(fun(arr,x,ans)==false){
+                    return false;
+                }
+            }else if(ans[i]==ans[x]){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& arr) {
+        int n=arr.size();
+        vector<int>ans(n,-1);
+        for(int i=0;i<n;i++){
+            if(ans[i]==-1){
+                if(fun(arr,i,ans)==false){
+                    return false;
                 }
             }
         }
