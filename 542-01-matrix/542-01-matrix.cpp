e4978fa6af1a6095fa139e65fa9493fecacf2ff4
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-11 08:55:08 +0530

    Time: 170 ms (17.94%), Space: 30.3 MB (50.54%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
-    
-    
-    bool isvalid(int i,int j,int m,int n)
-    {
-        if(i==m||j==n||j<0||i<0)
+    bool valid(int i,int j,int n,int m){
+        if(i<0 or j<0 or i>=n or j>=m){
             return false;
+        }
         return true;
     }
-    
-    vector<vector<int>> dir={{1,0},{0,1},{0,-1},{-1,0}};
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) 
-    {
-        queue<pair<int,int>> q;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<vector<int>> dis(m,vector<int>(n,-1));
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j]==0)
-                { 
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int,int>>q;
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,-1));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    ans[i][j]=0;
                     q.push({i,j});
-                    dis[i][j]=0;
-                }
-            }
-        while(!q.empty())
-        {
-            pair<int,int> curr=q.front();
-            q.pop();
-            for(auto& x:dir)
-            {
-                int a=curr.first+x[0];
-                int b=curr.second+x[1];
-                if(isvalid(a,b,m,n)&&dis[a][b]==-1)
-                {
-                    q.push({a,b});
-                    dis[a][b]=dis[curr.first][curr.second]+1;
                 }
             }
         }
-        return dis;
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        while(!q.empty()){
+            auto x=q.front();
+            int a=x.first;
+            int b=x.second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int x=a+dx[i];
+                int y=b+dy[i];
+                if(valid(x,y,n,m) and ans[x][y]==-1){
+                    ans[x][y]=ans[a][b]+1;
+                    q.push({x,y});
+                }
+            }
+        }
+        return ans;
     }
 };
