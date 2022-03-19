d600a2b01c084ebdeb119d7e42c247b15424b3da
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-19 15:04:11 +0530

    Time: 336 ms (80.40%), Space: 77.5 MB (51.37%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -10,12 +10,12 @@ public:
         sort(arr.begin(),arr.end(),cmp);
         int n=arr.size();
         vector<int> collector;
-	for(auto& pair: arr)
-	{
-		auto iter = lower_bound(collector.begin(), collector.end(), pair[1]);
-		if(iter == collector.end()) collector.push_back(pair[1]);
-		else if(*iter > pair[1]) *iter = pair[1];
-	}
-	return collector.size();
+        for(auto& pair: arr)
+        {
+            auto iter = lower_bound(collector.begin(), collector.end(),                     pair[1]);
+            if(iter == collector.end()) collector.push_back(pair[1]);
+            else if(*iter > pair[1]) *iter = pair[1];
+        }
+        return collector.size();
     }
 };
