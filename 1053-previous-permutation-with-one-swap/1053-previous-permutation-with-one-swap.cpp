f34a0b18c25324bce8341ce62a53f1c9a159cb69
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-25 09:58:44 +0530

    Time: 50 ms (22.03%), Space: 24.7 MB (41.53%) - LeetHub

--- 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.cpp
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     vector<int> prevPermOpt1(vector<int>& arr) {
         int n=arr.size();
-        for(int i=n-2;i>=0;i--){
-            int k=INT_MIN;
-            int c=-1;
-            for(int j=i+1;j<n;j++){
-                if(arr[i]>arr[j]){
-                    if(k<arr[j]){
-                    k=max(k,arr[j]);
-                    c=j;
-                    }
-                }
+        int i=n-2;
+        while(i>=0 ){
+            if(arr[i+1]<arr[i]){
+                break;
             }
-            if(k!=INT_MIN){
-                swap(arr[i],arr[c]);
-                return arr;
-            }
-            
+            i--;
         }
+        cout<<i<<endl;
+        if(i<0){
+            return arr;
+        }
+        int kk=INT_MIN;
+        int c=-1;
+        int in=i;
+        for(i=i+1;i<n;i++){
+            if(arr[i]<arr[in] and kk<arr[i] ){
+                kk=max(kk,arr[i]);
+                c=i;
+            }
+        }
+        swap(arr[in],arr[c]);
         return arr;
     }
 };
