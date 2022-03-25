63dc86ff6263ffd13b780f7e3619449265b756f0
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-25 15:01:27 +0530

    Time: 522 ms (25.89%), Space: 61.6 MB (12.33%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,16 +1,41 @@
+// class Solution {
+// public:
+//     int fun(vector<int>&nums,int i,int tar,int s){
+//         if(i==nums.size()){
+//             if(s==tar){
+//                 return 1;
+//             }
+//             return 0;
+//         }
+//         return fun(nums,i+1,tar,s+nums[i])+fun(nums,i+1,tar,s-nums[i]);
+//     }
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         return fun(nums,0,target,0);
+//     }
+// };
+
+
+
+// memeozize
+
 class Solution {
 public:
-    int fun(vector<int>&nums,int i,int tar,int s){
-        if(i==nums.size()){
-            if(s==tar){
+    map<pair<int,int>,int>dp;
+    int fun(int n,vector<int>& nums, int target){
+        if(n==0){
+            if(target==0){
                 return 1;
             }
             return 0;
         }
-        return fun(nums,i+1,tar,s+nums[i])+fun(nums,i+1,tar,s-nums[i]);
+        if(dp.find({n,target})!=dp.end()){
+            return dp[{n,target}];
+        }
+        return dp[{n,target}]=fun(n-1,nums,target+nums[n-1])+fun(n-1,nums,target-nums[n-1]);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        return fun(nums,0,target,0);
+        dp.clear();
+        return fun(nums.size(),nums,target);
     }
 };
