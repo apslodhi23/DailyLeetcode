ab9b543afc84aa1c9451bfa1c26db3f273afc1b1
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-27 11:49:25 +0530

    Time: 236 ms (100.00%), Space: 18.3 MB (16.67%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    
-    int func(vector<vector<int>>& p,int i,int k){
-        if(i==p.size()) 
-		return 0;
-
-	// if present in dp return
-        if(dp[i][k]!=-1) 
-		return dp[i][k];
-
-	//dont take from that pile
-        int ans=func(p,i+1,k);
-
-        int a=0;
-        for(int j=0;j<p[i].size()&&j<k;j++){
-		//take some certain prefix from that pile, if we can take that much elements.
-            a+=p[i][j];
-            ans=max(ans,a+func(p,i+1,k-j-1));
+    vector<vector<int>>dp;
+    int fun(int i,vector<vector<int>>&p,int k){
+        if(i==p.size()){
+            return 0;
         }
-		//return max
+        if(dp[i][k]!=-1){
+            return dp[i][k];
+        }
+        int ans=fun(i+1,p,k);
+        int a=0;
+        for(int j=0;j<p[i].size() and j<k ;j++){
+            a+=p[i][j];
+            ans=max(ans,a+fun(i+1,p,k-j-1));
+        }
         return dp[i][k]=ans;
     }
-
-    int maxValueOfCoins(vector<vector<int>>& p, int k) {
-
-        dp=vector<vector<int>>(p.size(),vector<int>(k+1,-1));
-
-        return func(p,0,k);
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        dp=vector<vector<int>>(piles.size()+1,vector<int>(k+1,-1));
+        return fun(0,piles,k);
     }
 };
