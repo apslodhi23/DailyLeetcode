50a5516947abb7c23f190966e6102aceac5dee42
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-28 10:44:58 +0530

    Time: 1330 ms (17.90%), Space: 106.7 MB (22.37%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     map<vector<int>,int>m;
-    int fun(int x,vector<int>& nums,vector<int>v){
+    int fun(int x,vector<int>&nums,vector<int>v){
         if(x>=nums.size()){
             return 0;
         }
@@ -10,17 +10,19 @@ public:
         }
         int ans=0;
         for(int i=0;i<v.size();i++){
-            if(v[i]>0){
-                v[i]--;
-                int temp=(nums[x]&(i+1))+fun(x+1,nums,v);
-                ans=max(ans,temp);
+            if(v[i]<2){
                 v[i]++;
+                ans=max(ans,( (nums[x]&(i+1)) +fun(x+1,nums,v)));
+                v[i]--;
             }
         }
         return m[v]=ans;
     }
     int maximumANDSum(vector<int>& nums, int n) {
-        vector<int>v(n,2);
+        // int n=nums.szie();
+        vector<int>v(n);
         return fun(0,nums,v);
+        
+        
     }
 };
