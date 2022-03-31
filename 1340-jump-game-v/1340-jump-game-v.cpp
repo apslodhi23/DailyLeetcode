3142a2925ae138108bc72aec145ad82bd01df6bb
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-03-31 10:02:38 +0530

    Time: 24 ms (97.95%), Space: 14.4 MB (92.56%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int dp[1001];
-    int fun(vector<int>&arr,int d,int i,int n){
+    int fun(vector<int>&arr,int d,int i,int val,int n){
         if(dp[i]!=-1){
             return dp[i];
         }
         int ans=0;
-        for(int j=i+1;j<=i+d and j<n ;j++){
-            if(arr[j]>=arr[i]){
+        for(int j=i+1;j<=min(i+d,n-1);j++){
+            if(arr[j]>=val){
                 break;
             }else{
-                ans=max(ans,fun(arr,d,j,n));
+                ans=max(ans, fun(arr,d,j,arr[j],n));
             }
         }
-        for(int j=i-1; j>=(i-d) and j>=0 ; j--){
-            if(arr[j]>=arr[i]){
+        for(int j=i-1;j>=max(0,i-d);j--){
+            if(arr[j]>=val){
                 break;
             }
             else{
-                ans=max(ans,fun(arr,d,j,n));
+                ans=max(ans ,fun(arr,d,j,arr[j],n));
             }
         }
         return dp[i]=ans+1;
@@ -27,7 +27,7 @@ public:
         int ans=0;
         memset(dp,-1,sizeof(dp));
         for(int i=0;i<arr.size();i++){
-            ans=max(ans,fun(arr,d,i,arr.size()));
+            ans=max(ans,fun(arr,d,i,arr[i],arr.size()));
         }
         return ans;
     }
