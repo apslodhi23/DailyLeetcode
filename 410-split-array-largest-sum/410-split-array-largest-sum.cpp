ae0a7cc302c08ce924079f2848efd4de7ebb8c09
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-04-01 10:43:47 +0530

    Time: 378 ms (19.06%), Space: 7.5 MB (27.66%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int dp[1001][51];
     int fun(vector<int>&arr,int i,int m){
-        if(m==0 and i==arr.size()){
+        if(m==0 and i>=arr.size()){
             return 0;
         }
-        if(m==0 or i==arr.size()){
+        
+        if(m==0 or i>=arr.size()){
             return INT_MAX;
         }
         if(dp[i][m]!=-1){
             return dp[i][m];
         }
+        int sum=0;
         int ans=INT_MAX;
-        int s=0;
         for(int j=i;j<arr.size();j++){
-            s+=arr[j];
-            int k=fun(arr,j+1,m-1);
-            int su=max(s,k);
-            ans=min(ans,su);
-        }
+            sum+=arr[j];
+            int k=max(sum,fun(arr,j+1,m-1));
+            ans=min(ans,k);
+       }
         return dp[i][m]=ans;
     }
     int splitArray(vector<int>& nums, int m) {
