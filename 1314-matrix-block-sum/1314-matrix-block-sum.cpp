55e7ae023ae1792f22a4c63ffd94de54e5c1a37f
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-04-09 10:08:46 +0530

    Time: 4 ms (98.62%), Space: 9.6 MB (16.71%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>pref(n+1,vector<int>(m+1,0));
+        vector<vector<int>>pref(n+1,vector<int>(m+1));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 pref[i][j]=pref[i-1][j]+pref[i][j-1]-pref[i-1][j-1]+mat[i-1][j-1];
@@ -14,10 +14,10 @@ public:
                 int r1=max(0,i-k),c1=max(0,j-k);
                 int r2=min(n-1,i+k),c2=min(m-1,j+k);
                 r1++;
-                r2++;
                 c1++;
+                r2++;
                 c2++;
-                mat[i][j]=pref[r2][c2]-pref[r2][c1-1]-pref[r1-1][c2]+pref[r1-1][c1-1];
+                mat[i][j]=pref[r2][c2]-pref[r1-1][c2]-pref[r2][c1-1]+pref[r1-1][c1-1];
             }
         }
         return mat;
