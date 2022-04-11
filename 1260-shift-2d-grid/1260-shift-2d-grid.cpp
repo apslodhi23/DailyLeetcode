499619b1407e91e44ae9d5184b3ea476eb57bc4a
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-04-11 09:30:49 +0530

    Time: 31 ms (74.64%), Space: 13.9 MB (73.22%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    // void fun(vector<vector<int>>&arr){
-    //     int n=arr.size();
-    //     int m=arr[0].size();
-    //     vector<int>
-    // }
+    void fun(vector<vector<int>>&arr,int l,int r){
+        int n=arr.size();
+        int m=arr[0].size();
+        while(l<r){
+            int lr=l/m, lc=l%m;
+            int rr=r/m,rc=r%m;
+            swap(arr[lr][lc],arr[rr][rc]);
+            l++;
+                r--;
+        }
+        
+    }
     vector<vector<int>> shiftGrid(vector<vector<int>>& arr, int k) {
         int n=arr.size();
         int m=arr[0].size();
-        int c=n*m;
-        k=k%c;
-        vector<int>v;
-        for(auto x:arr){
-            for(auto y:x){
-                v.push_back(y);
-            }
+        k=k%(n*m);
+        if(k==0){
+            return arr;
         }
-        int x=c-k;
-        // x--;
-        int y=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(x<c){
-                    arr[i][j]=v[x];
-                    x++;
-                }else{
-                    arr[i][j]=v[y];
-                    y++;
-                }
-            }
-        }
+        fun(arr,0,n*m-1);
+        fun(arr,0,k-1);
+        fun(arr,k,n*m-1);
         return arr;
     }
 };
