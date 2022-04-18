a29db4c3e30a9db9359cfb6f87b94af46ed2f56d
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-04-18 10:56:15 +0530

    Time: 230 ms (46.64%), Space: 90.4 MB (28.91%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int dp[100001][2][3];
-    int fun(vector<int>&arr,int c,int cap,int i){
-        if(i==arr.size()){
+    int fun(vector<int>&arr,int i,int c,int cap,int n){
+        if(i>=n){
             return 0;
         }
         if(cap==0){
@@ -11,14 +11,14 @@ public:
         if(dp[i][c][cap]!=-1){
             return dp[i][c][cap];
         }
-        if(c){
-            return dp[i][c][cap]= max(-arr[i]+fun(arr,0,cap,i+1), fun(arr,1,cap,i+1));
+        if(c==1){
+            return dp[i][c][cap]=max(-arr[i]+fun(arr,i+1,0,cap,n),fun(arr,i+1,1,cap,n));
         }else{
-            return dp[i][c][cap]=max(arr[i]+fun(arr,1,cap-1,i+1),fun(arr,0,cap,i+1));
+            return dp[i][c][cap]=max(arr[i]+fun(arr,i+1,1,cap-1,n),fun(arr,i+1,0,cap,n));
         }
     }
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int>& arr) {
         memset(dp,-1,sizeof(dp));
-        return fun(prices,1,2,0);
+        return fun(arr,0,1,2,arr.size());
     }
 };
