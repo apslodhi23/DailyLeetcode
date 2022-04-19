27514eb34fab071c60e30c5eb95655cd6791dc0f
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-04-19 09:24:24 +0530

    Time: 28 ms (90.57%), Space: 57.9 MB (47.12%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,30 +11,26 @@
  */
 class Solution {
 public:
-    vector<int>arr;
-    void fun1(TreeNode*root){
-        if(root==NULL){
-            return;
-        }
-        fun1(root->left);
-        arr.push_back(root->val);
-        fun1(root->right);
-    }
-    int i=0;
+    TreeNode*a,*b;
+    TreeNode*c=new TreeNode(INT_MIN);
     void fun(TreeNode*root){
         if(root==NULL){
             return;
         }
         fun(root->left);
-        root->val=arr[i];
-        i++;
+        if(a==NULL and c->val>root->val){
+            a=c;
+        }
+        if(a!=NULL and c->val>root->val){
+            b=root;
+        }
+        c=root;
         fun(root->right);
     }
     void recoverTree(TreeNode* root) {
-        arr.clear();
-        fun1(root);
-        sort(arr.begin(),arr.end());
-        i=0;
         fun(root);
+        int t=a->val;
+        a->val=b->val;
+        b->val=t;
     }
 };
