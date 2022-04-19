ad462564a620d1f2f5e383070631ac410db15242
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-04-19 10:05:31 +0530

    Time: 35 ms (51.54%), Space: 29.6 MB (6.33%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -1,27 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int ans=INT_MIN;
-    int fun(TreeNode*root){
+    int fun(TreeNode *root){
+        if(root->left==NULL && root->right==NULL){
+            ans=max(ans,root->val);
+            return root->val;
+        }
+        if(root->left==NULL){
+            int r=fun(root->right);
+            ans=max({ans,r,root->val,r+root->val});
+            return max({root->val+r,root->val});
+        }
+        if(root->right==NULL){
+            int l=fun(root->left);
+            ans=max({ans,l,root->val,l+root->val});
+            return max({root->val+l,root->val});
+        }
+        int l=fun(root->left);
+        int r=fun(root->right);
+        ans=max({ans,l+r+root->val,root->val,l+root->val,r+root->val});
+        return max({root->val+l,root->val+r,root->val});
+    }
+    int maxPathSum(TreeNode* root) {
         if(root==NULL){
             return 0;
         }
-        int l=max(fun(root->left),0);
-        int r=max(0,fun(root->right));
-        ans=max(ans,l+r+root->val);
-        return max(l,r)+root->val;
-    }
-    int maxPathSum(TreeNode* root) {
         fun(root);
         return ans;
     }
