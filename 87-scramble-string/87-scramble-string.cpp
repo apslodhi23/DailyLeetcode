efdbcf0f37f6d787a1e53a714895ad96bca025da
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-04-21 15:21:45 +0530

    Time: 654 ms (5.07%), Space: 30.3 MB (61.61%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -1,8 +1,9 @@
 class Solution {
-public:map<pair<string,string>,bool>mm;
+public:
+    map<pair<string,string>,bool>dp;
     bool fun(string s1,string s2){
-        int n=s1.length();
-        int m=s2.length();
+        int n=s1.size();
+        int m=s2.size();
         if(n!=m){
             return false;
         }
@@ -12,20 +13,21 @@ public:map<pair<string,string>,bool>mm;
         if(s1==s2){
             return true;
         }
-         if(mm.find({s1,s2})!=mm.end()){
-            return mm[{s1,s2}];
+        if(dp.find({s1,s2})!=dp.end()){
+            return dp[{s1,s2}];
         }
         for(int i=0;i<n-1;i++){
-            if(fun(s1.substr(0,i+1),s2.substr(n-i-1)) && fun(s1.substr(i+1),s2.substr(0,n-i-1))){
+            if(fun(s1.substr(0,i+1),s2.substr(0,i+1)) and fun(s1.substr(i+1),s2.substr(i+1))){
                 return true;
             }
-            if(fun(s1.substr(0,i+1),s2.substr(0,i+1)) && fun(s1.substr(i+1),s2.substr(i+1))){
+            if(fun(s1.substr(0,i+1),s2.substr(n-i-1)) and fun(s1.substr(i+1),s2.substr(0,n-i-1))){
                 return true;
             }
         }
-        return  mm[{s1,s2}]=false;
+        return dp[{s1,s2}]=false;
     }
     bool isScramble(string s1, string s2) {
+        dp.clear();
         return fun(s1,s2);
     }
 };
