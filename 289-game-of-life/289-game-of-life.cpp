1c798e5d4876a0ecc783f65d656588053bdc78e3
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-06-05 13:25:00 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (97.19%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,42 +1,45 @@
+//optimised soltuion
+//time: O(n*m);
+// space: O(1);
+
+
+
+//0 dead to dead
+//1 live to live
+//2 live to dead
+//3 daed to live
+
 class Solution {
 public:
     int dx[8]={1,-1,0,0,1,-1,-1,1};
     int dy[8]={0,0,1,-1,1,-1,1,-1};
-    int fun(vector<vector<int>>&arr,int i,int j,int n,int m){
-        int c=0;
-        for(int k=0;k<8;k++){
-            int x=i+dx[k];
-            int y=j+dy[k];
-            if(x<0 or x>=n or y<0 or y>=m){
-                continue;
-            }
-            if(arr[x][y]==1){
-                c++;
-            }
-        }
-        return c;
-    }
-    void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>>arr;
-        arr=board;
+    void gameOfLife(vector<vector<int>>& arr) {
         int n=arr.size();
         int m=arr[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                int c=fun(arr,i,j,n,m);
-                if(arr[i][j]==1){
-                    if(c<2 or c>3){
-                        board[i][j]=0;
-                    }else{
-                        board[i][j]=1;
+                int c=0;
+                for(int k=0;k<8;k++){
+                    int x=i+dx[k];
+                    int y=j+dy[k];
+                    if(x<0 or x>=n or y<0 or y>=m){
+                        continue;
                     }
-                }else{
-                    if(c==3){
-                        board[i][j]=1;
-                    }else{
-                        board[i][j]=0;
+                    if(arr[x][y]==1 or arr[x][y]==2){
+                        c++;
                     }
                 }
+                if(arr[i][j]==1 and (c<2 or c>3)){
+                    arr[i][j]=2;
+                }
+                else if(arr[i][j]==0 and c==3){
+                    arr[i][j]=3;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=arr[i][j]%2;
             }
         }
     }
