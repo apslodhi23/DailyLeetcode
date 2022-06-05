df2d6ae964f74627f31f6f79298c4e313c9b02a5
Aditya Pratap Singh Lodhi <99359083+apslodhi23@users.noreply.github.com>
2022-06-05 20:40:26 +0530

    Time: 3 ms (89.84%), Space: 9.2 MB (45.56%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    
-    bool static cmp(string &a,string &b){
-        return a.length()<b.length();
-    }
-    
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(),strs.end(),cmp);// sorting according to length
+        sort(strs.begin(),strs.end());
         
-        string ans=strs[0];
-        for(int i=1;i<strs.size();i++){
-            int c=0;
-            for(int j=0;j<ans.size();j++){
-                if(ans[j]!=strs[i][j]){
-                    break;
-                }
-                c++;
+        string ans="";
+        for(int i=0;i<strs[0].size();i++){
+            if(strs[0][i]!=strs[strs.size()-1][i]){
+                break;
             }
-            ans=ans.substr(0,c);
+            ans+=strs[0][i];
         }
         return ans;
-        
     }
 };
